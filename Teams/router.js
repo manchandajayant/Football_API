e78ff3d8e7ed60d9dd65dfdc07b1ace2a0548548
manchandajayant <maanjayant@gmail.com>
2020-02-26 17:07:29 +0100

    find team by unique  id added

--- Teams/router.js
+++ Teams/router.js
@@ -3,7 +3,7 @@ const Team = require("./model");
 const router = new Router();
 
 router.get("/team", (req, res, next) => {
-  console.log("this is getting them", attributes);
+  //console.log("this is getting them", attributes);
   Team.findAll({ attributes: ["name"], raw: true })
     .then(teams => {
       res.json(teams);
@@ -15,6 +15,14 @@ router.post("/team", (req, res, next) => {
   console.log("this is a post", req.body);
   Team.create(req.body)
     .then(team => res.json(team))
+    .catch(next);
+});
+
+router.get("/team/:id", (req, res, next) => {
+  Team.findByPk(req.params.id)
+    .then(user => {
+      res.json(user);
+    })
     .catch(next);
 });
 
