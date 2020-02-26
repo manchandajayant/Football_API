2076d937ebbeb3a67e46ce61593ebfac683576b8
manchandajayant <maanjayant@gmail.com>
2020-02-26 15:57:04 +0100

    databse update message added

---
+++ db.js
@@ -0,0 +1,11 @@
+const Sequelize = require("sequelize");
+const databaseUrl =
+  process.env.DATABASE_URL ||
+  "postgres://postgres:jayant@localhost:5432/postgres";
+const db = new Sequelize(databaseUrl);
+
+db.sync()
+  .then(() => console.log("congratulations, you were finally able to do this"))
+  .catch(console.error);
+
+module.exports = db;
